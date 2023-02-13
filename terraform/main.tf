35d47d84e9dfde79869b0a6311338625ffd24cea
MakariosNassef <makarios059@gmail.com>
2023-02-13 21:05:28 +0200

    terraform files

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+module "network_module" {
+  source = "./network"
+  main = "main-vpc"
+
+  # management-subnet-name = "management-subnet"
+  # restricted-subnet-name = "restricted-subnet"
+}
+
+module "cluster_module" {
+  source = "./cluster"
+  private_us_east_1a_id = module.network_module.private_us_east_1a_id_output
+  private_us_east_1b_id = module.network_module.private_us_east_1b_id_output
+  public_us_east_1a_id = module.network_module.public_us_east_1a_id_output
+  public_us_east_1b_id = module.network_module.public_us_east_1b_id_output
+}
+
---
+++ terraform/provider.tf
@@ -0,0 +1,6 @@
+
+provider "aws"{
+  shared_config_files      = ["/home/mac/.aws/config"]
+  shared_credentials_files = ["/home/mac/.aws/credentials"]
+  profile                  = ""
+}
