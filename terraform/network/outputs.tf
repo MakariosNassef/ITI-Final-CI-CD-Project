f1bc7141a7669bdba5c276ec2a079e9f7a428710
MakariosNassef <makarios059@gmail.com>
2023-02-13 21:11:51 +0200

    edit terraform files

---
+++ terraform/network/outputs.tf
@@ -0,0 +1,15 @@
+output "private_us_east_1a_id_output" {
+  value = aws_subnet.private_us_east_1a.id
+}
+
+output "private_us_east_1b_id_output" {
+  value = aws_subnet.private_us_east_1b.id
+}
+
+output "public_us_east_1a_id_output" {
+  value = aws_subnet.public_us_east_1a.id
+}
+
+output "public_us_east_1b_id_output" {
+  value = aws_subnet.public_us_east_1b.id
+}
---
+++ terraform/network/variables.tf
@@ -0,0 +1,19 @@
+variable "main" {
+    description = "aws_Name"
+}
+
+# variable "private_us_east_1a_id" {
+#   //value = aws_subnet.private_us_east_1a.id
+# }
+
+# variable "private_us_east_1b_id" {
+#   //value = aws_subnet.private_us_east_1b.id
+# }
+
+# variable "public_us_east_1a_id" {
+#   //value = aws_subnet.public_us_east_1a.id
+# }
+
+# variable "public_us_east_1b_id" {
+#   //value = aws_subnet.public_us_east_1b.id
+# }
